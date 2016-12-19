7305834e7453c437b23575defe716b35758581c2
kaimhart <kaimhart55@gmail.com>
2016-12-19 15:49:31 -0500

    Added a Chaser class
    + Added a Chaser.js class file
    + Added a linke to Chaser.js in index.html
    + Added codes in sketch.js to draw a Chaser

---
+++ chaser.js
@@ -0,0 +1,5 @@
+function Chaser() {
+  this.show = function() {
+    ellipse(width/2, height/2, 10, 10);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+var chaser;
+
 function setup() {
   createCanvas(400, 400);
+  chaser = new Chaser();
 }
 
 function draw() {
   background(50);
+  chaser.show();
 }
