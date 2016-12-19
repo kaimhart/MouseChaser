5824bb0a463149532cb9829f87582b48d3f78302
kaimhart <kaimhart55@gmail.com>
2016-12-19 16:21:23 -0500

    Added vectors to chaser.js

--- chaser.js
+++ chaser.js
@@ -1,6 +1,20 @@
 function Chaser() {
+  this.location = createVector(width/2, height/2);
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
+
   this.show = function() {
-    ellipse(width/2, height/2, 10, 10);
-    //
+    ellipse(this.location.x, this.location.y, 10, 10);
+  }
+
+  this.update = function() {
+    var mouse = createVector(mouseX, mouseY);
+
+    mouse.sub(location);
+    mouse.setMag(0.8);
+    this.acceleration = mouse;
+    this.velocity.add(this.acceleration);
+    this.location.add(this.velocity);
+    this.velocity.limit(5);
   }
 }
